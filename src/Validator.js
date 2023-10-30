a3cc882bfae27c4336ff802bbae6fccb17596cc3
inariik <egorchekushin@gmail.com>
2023-10-31 02:12:44 +0500

    test

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,17 @@
+import ArraySchema from './ArraySchema.js';
+import ObjectSchema from './ObjectSchema.js';
+import StringSchema from './StringSchema.js';
 
+export default class Validator {
+  string() {
+    return new StringSchema([(value) => typeof value === 'string']);
+  }
+
+  array() {
+    return new ArraySchema([(value) => Array.isArray(value)]);
+  }
+
+  object() {
+    return new ObjectSchema();
+  }
+}
